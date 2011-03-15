f8e81c247c8a5ab26e6f4bf22ddd0fc4429ef046
Dan Scanlon <dscanlon@idlize.com>
2011-03-15 13:50:39 -0400

    first commit

---
+++ Native/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Native::Application.config.secret_token = 'b3cbdb3fe68540f8a656c93afa35ad16e12bc20f13561d814f19387536c828102d4cf326e284b3faaf9d9720e91340e9d8598afaa63e05d69bd30a430065db17'
